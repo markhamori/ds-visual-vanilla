88dccb8afd7de0372adaf166ed1c53f0b9a33784
Márk Hámori <hamorimark@hotmail.com>
2022-05-15 12:50:08 +0200

    Ds progress

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 import "./css/styles.css";
-import { SLL_getNodes, SLL_addNode } from "./methods/sll.js";
-
-const color1 = document.querySelector(".color-1");
-const color2 = document.querySelector(".color-2");
-const color3 = document.querySelector(".color-3");
-const color4 = document.querySelector(".color-4");
-const color5 = document.querySelector(".color-5");
-const color6 = document.querySelector(".color-6");
-const color7 = document.querySelector(".color-7");
-const color8 = document.querySelector(".color-8");
-const color9 = document.querySelector(".color-9");
-const color10 = document.querySelector(".color-10");
-const color11 = document.querySelector(".color-11");
-const nodes = document.querySelectorAll(".nodes");
+import {
+  SLL_getNodes,
+  SLL_pushNode,
+  SLL_popNode,
+  SLL_shiftNode,
+  SLL_unshiftNode,
+} from "./methods/sll.js";
 
 const sll = true;
+const methods = document.querySelector(".methods");
 
 if (sll == true) {
   SLL_getNodes();
+
+  const pushBtn = document.createElement("button");
+  const pushInput = document.createElement("input");
+  const pushLabel = document.createElement("label");
+
+  pushBtn.innerText = "Push";
+  pushInput.classList.add("balloon");
+  pushInput.id = "galaxy";
+  pushLabel.setAttribute("for", pushInput.id);
+  pushLabel.htmlFor = "galaxy";
+  pushLabel.innerHTML = "Push";
+
+  methods.appendChild(pushBtn);
+  methods.appendChild(pushInput);
 }
--- src/methods/sll.js
+++ src/methods/sll.js
@@ -5,16 +5,27 @@ sll.push(5);
 sll.push(10);
 sll.push(20);
 sll.push(8);
+sll.push(30);
 
 const dsTitle = document.querySelector(".data-structure-title");
-
 const nodeContainer = document.querySelector(".node-container");
-const nodeValue = document.querySelector(".node-value");
 
 dsTitle.textContent = "Singly Linked List";
 
-export function SLL_addNode(value) {
+export function SLL_pushNode(value) {
   sll.push(value);
+}
+
+export function SLL_popNode() {
+  sll.pop();
+}
+
+export function SLL_shiftNode() {
+  sll.shift();
+}
+
+export function SLL_unshiftNode(value) {
+  sll.unshift(value);
 }
 
 export function SLL_getNodes() {
@@ -22,6 +33,7 @@ export function SLL_getNodes() {
     const div = document.createElement("div");
     div.classList.add("nodes");
     div.classList.add(`node-${node.val}`);
+
     if (sll.findAll()[0].val === node.val) {
       div.innerText = `${node.val} \n Head \n Next: ${node.next.val}`;
     } else if (sll.findAll()[sll.findAll().length - 1].val === node.val) {
@@ -29,7 +41,10 @@ export function SLL_getNodes() {
     } else {
       div.innerText = `${node.val} \n Next: ${node.next.val}`;
     }
+    const arrow = document.createElement("div");
+    arrow.classList.add("single-arrow");
 
     nodeContainer.append(div);
+    if (node.next != null) nodeContainer.append(arrow);
   }
 }
