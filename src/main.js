3044a8d846787b459386fe357787fd3e50c27495
Márk Hámori <hamorimark@hotmail.com>
2022-05-19 16:32:56 +0200

    Ui

--- src/main.js
+++ src/main.js
@@ -9,8 +9,12 @@ const sllBtn = document.getElementById("sll");
 const dllBtn = document.getElementById("dll");
 const stacksBtn = document.getElementById("stacks");
 const dsTitle = document.querySelector(".data-structure-title");
+const openBtn = document.querySelector(".nav__open");
+const openBtnImg = document.querySelector(".nav__open").children[0];
+const navigation = document.getElementsByTagName("nav")[0];
 
 let randomNum = Math.random() * 6 + 1;
+let open = false;
 dsTitle.textContent = "Select a structure";
 
 sllBtn.addEventListener("click", () => {
@@ -23,6 +27,17 @@ dllBtn.addEventListener("click", () => {
 
 stacksBtn.addEventListener("click", () => {
   STACKS_events();
+});
+
+openBtn.addEventListener("click", () => {
+  open = !open;
+  if (open) {
+    openBtnImg.src = "./src/assets/circle-chevron-right-solid.svg";
+    navigation.style.right = 0;
+  } else {
+    openBtnImg.src = "./src/assets/circle-chevron-left-solid.svg";
+    navigation.style.right = "-360px";
+  }
 });
 
 const logKey = (e) => {
